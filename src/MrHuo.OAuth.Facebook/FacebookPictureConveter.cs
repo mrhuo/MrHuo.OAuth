0d91ee5681d60be50807fdefb447184a8344e352
Donma Hsu <no2don@gmail.com>
2021-01-25 17:22:46 +0800

    add facebook oauth

---
+++ src/MrHuo.OAuth.Facebook/FacebookPictureConveter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using static MrHuo.OAuth.Facebook.FacebookUserModel;
+
+namespace MrHuo.OAuth.Facebook
+{
+    public class FacebookPictureConveter : JsonConverter<PictureInfo>
+    {
+
+
+        public override PictureInfo Read(ref Utf8JsonReader reader, Type type, JsonSerializerOptions options)
+        {
+            return JsonSerializer.Deserialize<PictureInfo>(ref reader);
+        }
+
+
+        public override void Write(Utf8JsonWriter writer, PictureInfo value, JsonSerializerOptions options)
+        {
+            JsonSerializer.Serialize(writer, value);
+        }
+    }
+}
